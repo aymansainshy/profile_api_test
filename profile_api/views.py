fae36fad149cd3dd97fb34d85d58598d1ed52a40
aymansainshy <aymansainshy@gmail.com>
2020-11-05 15:25:38 +0300

    my first api call

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test HelloApiView"""
+
+    def get(self, request, format=None):
+        an_apiView = [ 'ayman','mohammed','Seddig', 'ayman sainshy # gmail.com']
+        return Response({'message' : 'Hello', 'an_apiView':an_apiView})
+
