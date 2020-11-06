3f89dba38bfae3bfa22688068efd1e25d3217f1d
aymansainshy <aymansainshy@gmail.com>
2020-11-06 09:12:37 +0300

    add ViewSet to our Profile Api

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
 from profile_api import serializers
 
-
+""" ////////////////////////////////////////////// [ ApiView ] //////////////////////////////////////////////// """
 class HelloApiView(APIView):
     """Test HelloApiView"""
     serializers_calss = serializers.HelloSerializer
-
 
     def get(self, request, format=None):
         an_apiView = [ 'ayman','mohammed','Seddig', 'ayman sainshy # gmail.com']
@@ -26,6 +26,57 @@ class HelloApiView(APIView):
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
-            )    
+            ) 
+
+    def put(self,request,pk=None):          
+         """Hendel updating an Object"""
+         return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):          
+         """Hendel updating partial an Object"""
+         return Response({'method':'PATCH'})
+
+    def delet(self,request,pk=None):          
+         """Delete an Object"""
+         return Response({'method':'DELETE'})
 
 
+""" ////////////////////////////////////////////// [ ViewSet ] //////////////////////////////////////////////// """
+class HelloViewSet(viewsets.ViewSet):
+    """Test ApiviewSet """
+    serializers_calss = serializers.HelloSerializer 
+
+    def list(self,request):
+        """Return Hello message"""
+        a_viewSet = [ 'ayman','mohammed','Seddig', 'ayman sainshy # gmail.com' ,'Adam', 'i am actually working at BDC as a mobile app Developer !']
+        return Response({'message':'Hello !' ,'a_viewSet':a_viewSet})
+  
+    def create(self,request):
+        """Create a new hello message """
+        serializer = self.serializers_calss(data=request.data)
+
+        if serializer.is_valid():
+            name    = serializer.validated_data.get('name')
+            message = f'Hello {name} !'
+            return Response({'message' : message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )   
+
+    def retrieve(self, request, pk=None):
+        """Hendel getting an Object by it's Id """
+        return Response({'http_Method' : 'GET'})         
+   
+    def update(self, request, pk=None):
+        """Hendel Updating an Object by it's Id """
+        return Response({'http_Method' : 'PUT'})         
+   
+    def partial_update(self, request, pk=None):
+        """Hendel Updating part of an Object by it's Id """
+        return Response({'http_Method' : 'PATCH'})         
+    
+    def destroy(self, request, pk=None):
+        """Hendel deleting an Object by it's Id """
+        return Response({'http_Method' : 'DELETE'})         
